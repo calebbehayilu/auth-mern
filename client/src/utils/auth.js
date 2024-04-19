a04ef345308660ad32ffb15970e470e2513c467e
Caleb Behayilu <calebbehayilu@gmail.com>
2024-04-19 04:20:59 -0700

    vercel update

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
+const url = import.meta.env.VITE_APP_API_URL;
+
 export async function login(user) {
   try {
-    const res = await axios.post("http://localhost:3000/auth", {
+    const res = await axios.post(`${url}/auth`, {
       email: user.email,
       password: user.password,
     });
     if (res.statusText !== "OK") {
       console.log(res);
     }
-
+    console.log(res);
     localStorage.setItem("token", res.headers["x-auth-token"]);
 
     return res;
