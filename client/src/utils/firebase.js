f7314174fde68700b9f0e85982932db4e23a7dba
Caleb Behayilu <calebbehayilu@gmail.com>
2024-05-16 04:18:47 -0700

    🐛 FIX: Another Fix.

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import "firebase/storage";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,4 +23,4 @@ export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-export const storage = app.storage();
+export const storage = getStorage(app);
