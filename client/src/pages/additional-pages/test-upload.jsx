a6bab21386707f84b38421aafda4ba777c8a8775
Caleb Behayilu <calebbehayilu@gmail.com>
2024-05-16 04:12:58 -0700

    🐛 FIX: File Upload.

--- client/src/pages/additional-pages/test-upload.jsx
+++ client/src/pages/additional-pages/test-upload.jsx
@@ -5,27 +5,27 @@ const FileUpload = () => {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
+    // const selectedFile = e.target.files[0];
+    // setFile(selectedFile);
   };
 
   const handleUpload = () => {
-    if (file) {
-      const uploadTask = storage.ref(`pdfs/${file.name}`).put(file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Track upload progress
-        },
-        (error) => {
-          console.error("Error uploading file:", error);
-        },
-        () => {
-          // Handle successful upload
-          console.log("File uploaded successfully");
-        }
-      );
-    }
+    // if (file) {
+    //   const uploadTask = storage.ref(`pdfs/${file.name}`).put(file);
+    //   uploadTask.on(
+    //     "state_changed",
+    //     (snapshot) => {
+    //       // Track upload progress
+    //     },
+    //     (error) => {
+    //       console.error("Error uploading file:", error);
+    //     },
+    //     () => {
+    //       // Handle successful upload
+    //       console.log("File uploaded successfully");
+    //     }
+    //   );
+    // }
   };
 
   return (
